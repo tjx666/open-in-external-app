41d789f78408ab7ca4c1d0d82f711ab4159e7385
YuTengjing <ytj2713151713@gmail.com>
2019-12-11 23:26:18 +0800

    :construction_worker: build: add HardSourceWebpackPlugin

--- configs/webpack.common.ts
+++ configs/webpack.common.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { Configuration } from 'webpack';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ProgressBarPlugin from 'progress-bar-webpack-plugin';
+import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
 
 const commonWebpackConfig: Configuration = {
     target: 'node',
@@ -28,7 +29,13 @@ const commonWebpackConfig: Configuration = {
             },
         ],
     },
-    plugins: [new ProgressBarPlugin(), new CleanWebpackPlugin()],
+    plugins: [
+        new ProgressBarPlugin(),
+        new CleanWebpackPlugin(),
+        new HardSourceWebpackPlugin({
+            info: { mode: 'none', level: 'warn' },
+        }),
+    ],
 };
 
 export default commonWebpackConfig;
