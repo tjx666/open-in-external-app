ef461d2619b9b56905e4836ebb97ca50dd1809e4
YuTengjing <ytj2713151713@gmail.com>
2019-12-12 17:18:35 +0800

    :white_check_mark: test: add test for config.ts

---
+++ test/config.test.ts
@@ -0,0 +1,42 @@
+import assert from 'assert';
+import { validateConfiguration } from '../src/config';
+
+describe('#config', () => {
+    describe('#validateConfiguration', () => {
+        it(`#should error as config can't object`, () => {
+            assert(validateConfiguration({} as any).error !== null);
+        });
+
+        it(`#should error as apps field can't be ignored`, () => {
+            const configuration: any = [{ extensionName: 'html' }];
+            assert(validateConfiguration(configuration).error !== null);
+        });
+
+        it(`should pass validation`, () => {
+            const configuration: any = [
+                {
+                    extensionName: 'html',
+                    apps: [
+                        {
+                            title: 'ff',
+                            openCommand: '/a/b/firefox.exe',
+                        },
+                        {
+                            title: 'chrome',
+                            openCommand: '/a/b/chrome.exe',
+                        },
+                    ],
+                },
+                {
+                    extensionName: 'tsx',
+                    apps: 'code',
+                },
+                {
+                    extensionName: 'md',
+                    apps: 'sublime',
+                },
+            ];
+            assert(validateConfiguration(configuration).error === null);
+        });
+    });
+});
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,33 +1,29 @@
 import assert from 'assert';
-import * as vscode from 'vscode';
-import { iSRepeat } from '../src/util';
+import { isRepeat } from '../src/util';
 
 describe('# test sample', () => {
-    before(() => {
-        vscode.window.showInformationMessage('Test begin!');
-    });
-
     describe('#isRepeat', () => {
         it('#should isRepeat([]) return false', () => {
-            assert(!iSRepeat([]));
+            assert(!isRepeat([]));
         });
 
         it('#should isRepeat([1, 2, 1]) return true', () => {
-            assert(iSRepeat([1, 2, 1]));
+            assert(isRepeat([1, 2, 1]));
+        });
+
+        it('#should return false as objects are not equals using ===', () => {
+            const testArray = [{ name: 'ly' }, { name: 'lily' }, { name: 'ly' }];
+            assert(!isRepeat(testArray));
         });
 
         it('#should return false as not includes two same Object', () => {
             const testArray = [{ name: 'ly' }, { name: 'lily' }];
-            assert(!iSRepeat(testArray, (a, b) => a.name === b.name));
+            assert(!isRepeat(testArray, (a, b) => a.name === b.name));
         });
 
         it('#should return true as includes two same Object', () => {
             const testArray = [{ name: 'ly' }, { name: 'lily' }, { name: 'ly' }];
-            assert(iSRepeat(testArray, (a, b) => a.name === b.name));
+            assert(isRepeat(testArray, (a, b) => a.name === b.name));
         });
-    });
-
-    after(() => {
-        vscode.window.showInformationMessage('Test end!');
     });
 });
